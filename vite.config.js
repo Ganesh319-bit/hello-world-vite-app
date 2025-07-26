d8c7774f5ee04a643ac0aa3c5ad96853d85490e2
Ganesh319-bit <2300090319csit@gmail.com>
2025-07-26 10:08:53 +0530

    Fix: clean vite.config.js with correct base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-
 export default defineConfig({
-  base: '/hello-world-vite-app/', 
+  base: '/hello-world-vite-app/',
   plugins: [react()],
 });
